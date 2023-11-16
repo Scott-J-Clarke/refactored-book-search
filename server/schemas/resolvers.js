62e08a1920522645d7328e0d21b28d3514c9de4f
Vivid Quill <sjdoublec@gmail.com>
2023-11-16 10:41:18 -0600

    Tried to set up Apollo Server in 'server.js' and 'package.json'

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,10 +2,35 @@ const { User, Book } = require('../models');
 
 const resolvers = {
     Query: {
-        user: async () => {
-            return User.find({});
+        userLogin: async () => {
+            return userLogin.findOne({ $or: [{ username: body.username}, { email: body.email }] });
+        },
+        getSingleUser: async (parent, {userId}) => {
+            const params = userId ? { userId } : {};
+            return User.find(params);
+        },
+    },
+    Mutation: {
+        createUser: async (parent, args) => {
+            const newUser = await User.create(args);
+            return newUser;
+        },
+        saveBook: async (parent, { bookId }) => {
+            const savedBook = await Book.findOneAndUpdate(
+                { bookId },
+                { new: true }
+            );
+            return savedBook;
+        },
+        deleteBook: async (parent, { bookId }) => {
+            const deleteBook = await Book.findOneAndDelete(
+                { bookId }
+            );
+            return deleteBook
         }
     }
-}
+};
 
 module.exports = resolvers;
+
+// Should there be something like get book by id?
