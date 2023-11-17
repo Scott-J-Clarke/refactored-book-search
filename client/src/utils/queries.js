c577cc794f63a00a8d6267a50d2f20dc2860eacd
Vivid Quill <sjdoublec@gmail.com>
2023-11-17 08:53:22 -0600

    Wrote client-side 'queries.js'

---
+++ client/src/utils/queries.js
@@ -0,0 +1,47 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+    query user {
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks
+        }
+    }
+`;
+export const QUERY_BOOKS = gql`
+    query books($bookId: String) {
+        books(bookId: $bookId) {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`;
+
+export const GET_AUTH = gql`
+    query auth {
+        auth {
+            token
+            user {
+                id
+                username
+                email
+            }
+        }
+    }
+`;
+export const QUERY_ME = gql`
+    query me {
+        me {
+            id
+            username
+            email
+        }
+    }
+`;
